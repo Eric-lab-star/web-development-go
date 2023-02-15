acde4f0fda4926d0d3f22aa438297cc256395044
eric-lab-star <cyon256@icloud.com>
2023-02-15 11:37:08 +0900

    create gitignore file

--- main.go
+++ main.go
@@ -2,42 +2,18 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 )
 
-func renderTemplate(w http.ResponseWriter, r *http.Request) {
-	parsedTemplate, _ := template.ParseFiles("form.html")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		log.Println("Error executing template :", err)
-		return
-	}
-
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(r.Method)
-	fmt.Println(r.URL)
-	fmt.Println(r.URL)
-
-	fmt.Fprintln(w, "Email : ", r.PostForm.Get("email"))
-	fmt.Fprintln(w, "Password : ", r.PostForm.Get("password"))
-	fmt.Fprintln(w, "Remember Me : ", r.PostForm.Get("remember_check"))
-
-}
+var PORT = "3000"
 
 func main() {
-	http.HandleFunc("/", renderTemplate)
-	http.HandleFunc("/login", login)
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		log.Fatal("Error Starting the HTTP Server : ", err)
-		return
-	}
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Listening to server on http://localhost:3000")
+	http.ListenAndServe("localhost:"+PORT, nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to go server")
 }
