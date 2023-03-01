3eb37dc2b93db40fa4a36da295f7a3e615b6394d
eric-lab-star <cyon256@icloud.com>
2023-03-01 13:07:12 +0900

    template

--- server/main.go
+++ server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
@@ -11,33 +14,38 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", home)
-	r.Get("/user/{userId}", profile)
+	r.Get("/contact", contact)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		notFound(w)
 	})
-	apiRouter := chi.NewRouter()
-	apiRouter.Get("/books", getBooks)
-	r.Mount("/api", apiRouter)
 
 	http.ListenAndServe("localhost:8080", r)
 }
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	render(w, "books")
-}
-
-func profile(w http.ResponseWriter, r *http.Request) {
-	userId := chi.URLParam(r, "userId")
-	render(w, "userId: "+userId)
-}
-
 func home(w http.ResponseWriter, r *http.Request) {
-	render(w, "home")
+	file := filepath.Join("template", "home.gohtml")
+	render(w, file)
 }
 
-func render(w http.ResponseWriter, v string) {
+func contact(w http.ResponseWriter, r *http.Request) {
+	file := filepath.Join("template", "contact.gohtml")
+	render(w, file)
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	file := filepath.Join("template", "faq.gohtml")
+	render(w, file)
+}
+
+func render(w http.ResponseWriter, file string) {
+
 	w.Header().Set("Content-Type", "text/html; charset=utf8")
-	w.Write([]byte(v))
+	tpl, err := template.ParseFiles(file)
+	if err != nil {
+		log.Printf("error: %s", file)
+		http.Error(w, "Parsing template error", http.StatusInternalServerError)
+	}
+	tpl.Execute(w, nil)
 }
 func notFound(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
