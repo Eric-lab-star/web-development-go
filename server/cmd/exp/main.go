070f19495f4778a10bece14200df4e5d7011253b
eric-lab-star <cyon256@icloud.com>
2023-03-01 10:38:32 +0900

    template

---
+++ server/cmd/exp/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	data := struct{ Name string }{
+		Name: "Kim Kyungsub",
+	}
+	t.Execute(os.Stdout, data)
+}
