eb6ff2f4f52216ef085a72d73212e8ba8cd85382
eric-lab-star <cyon256@icloud.com>
2023-03-05 14:39:05 +0900

    create template

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/Eric-lab-star/web-development/server/views"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
-func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", home)
-	r.Get("/contact", contact)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		notFound(w)
-	})
-
-	http.ListenAndServe("localhost:8080", r)
+func render(w http.ResponseWriter, filePath string) {
+	tpl, err := views.Parse(filePath)
+	if err != nil {
+		log.Printf("error parsing template from file path: %s", filePath)
+		http.Error(w, "Parsing template error", http.StatusInternalServerError)
+	}
+	tpl.Execute(w, nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -37,18 +34,20 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	render(w, file)
 }
 
-func render(w http.ResponseWriter, file string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf8")
-	tpl, err := template.ParseFiles(file)
-	if err != nil {
-		log.Printf("error: %s", file)
-		http.Error(w, "Parsing template error", http.StatusInternalServerError)
-	}
-	tpl.Execute(w, nil)
-}
-
 func notFound(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
 	w.Header().Set("Content-Type", "text/html; charset=utf8")
 	w.Write([]byte("<h1>Page Not Found</h1>"))
+}
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", home)
+	r.Get("/contact", contact)
+	r.Get("/faq", faq)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		notFound(w)
+	})
+	log.Println("Server is listening to https://localhost:8080.")
+	http.ListenAndServe("localhost:8080", r)
 }
